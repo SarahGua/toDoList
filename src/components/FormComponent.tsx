c4eec26e4e5ffe2fb234e35f0732c5a79641d044
SarahGuarneri <sarahguarneri97@icloud.com>
2024-04-18 18:26:20 +0200

    fetch get and post

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,15 +1,63 @@
+import { useState } from "react"
 import { Button, Form } from "react-bootstrap"
 
 const FormComponent = () => {
+
+    const [formData, setFormData] = useState({
+        title: "",
+        description: ""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }))
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        fetch('http://localhost:3001/notes', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        })
+        .then((res) => {
+            if(res.ok){
+                setFormData({title: '', description: ''})
+                window.location.reload();
+            } else {
+                throw new Error('errore nel post')
+            }
+        })
+        .catch((err) => console.log(err))
+    }
+
     return (
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3">
-                    <Form.Control type="text" placeholder="Title" />
+                    <Form.Control 
+                    type="text" 
+                    placeholder="Title"
+                    name= 'title'
+                    value={formData.title}
+                    onChange={handleChange}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3">
-                    <Form.Control type="text" placeholder="Description" />
+                    <Form.Control 
+                    type="text" 
+                    placeholder="Description"
+                    name= 'description'
+                    value={formData.description}
+                    onChange={handleChange}
+                    />
                 </Form.Group>
                 
                 <div className="d-flex justify-content-center">
--- src/components/SingleListComponent.tsx
+++ src/components/SingleListComponent.tsx
@@ -1,14 +1,66 @@
-import { ListGroup } from "react-bootstrap"
+import { useEffect, useState } from "react"
+import { Col, ListGroup, Row } from "react-bootstrap"
+
+export interface Note {
+    id: number;
+    title: string;
+    description: string;
+}
+
+export interface NoteList {
+    success: boolean;
+    data: Note[];
+}
 
 const SingleListComponent = () => {
+
+    const [notes, setNotes] = useState<NoteList>()
+
+    useEffect(() => {
+        fetch('http://localhost:3001/notes')
+        .then((res) => {
+            if(res.ok){
+                return res.json()
+            } else {
+                throw new Error('errore nella fetch')
+            }
+        })
+        .then((data) => {
+            console.log(data)
+            setNotes(data)
+        })
+        .catch((err) => console.log('errore nel recupero dati', err))
+    }, [])
+
     return (
         <div>
             <ListGroup as="ul" className="mt-5">
-                <ListGroup.Item as="li" className="d-flex align-items-center justify-content-between">
-                    <h5 className="m-0">Title</h5>
-                    <p className="m-0">Description</p>
-                    <p className="m-0">DELETE</p>
-                </ListGroup.Item>
+                {
+                    notes && (
+                        notes.data.map((note) => {
+                            console.log('ecco le notes', notes)
+                            return (
+                                <ListGroup.Item as="li" key={note.id} className="mb-1">
+                                    <Row>
+                                        <Col>
+                                            <Row>
+                                                <Col>
+                                                    <h5 className="m-0">{note.title}</h5>
+                                                </Col>
+                                                <Col className="d-flex justify-content-center">
+                                                    <p className="m-0">{note.description}</p>
+                                                </Col>
+                                                <Col className="d-flex justify-content-end">
+                                                    <p className="m-0">DELETE</p>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            )
+                        })
+                    )
+                }
             </ListGroup>
         </div>
     )
